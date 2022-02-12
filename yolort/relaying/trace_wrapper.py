97c57d6b76b05675a80ccead1d75af6f8ca2d6cd
Zhiqiang Wang <zhiqwang@foxmail.com>
2022-02-12 12:30:03 +0800

    Use input_sample parameter in get_trace_module

--- yolort/relaying/trace_wrapper.py
+++ yolort/relaying/trace_wrapper.py
@@ -1,8 +1,45 @@
-# Copyright (c) 2021, Zhiqiang Wang. All Rights Reserved.
-from typing import Dict, Tuple, Callable
+# Copyright (c) 2021, yolort team. All rights reserved.
+
+from typing import Dict, Callable, Optional, Tuple
 
 import torch
 from torch import nn, Tensor
+
+
+@torch.no_grad()
+def get_trace_module(model_func: Callable[..., nn.Module], input_sample: Optional[Tensor] = None):
+    """
+    Get the tracing of a given model function.
+
+    Example:
+
+        >>> from yolort.models import yolov5s
+        >>> from yolort.relaying.trace_wrapper import get_trace_module
+        >>>
+        >>> model = yolov5s(pretrained=True)
+        >>> tracing_module = get_trace_module(model)
+        >>> print(tracing_module.code)
+        def forward(self,
+            x: Tensor) -> Tuple[Tensor, Tensor, Tensor]:
+          _0, _1, _2, = (self.model).forward(x, )
+          return (_0, _1, _2)
+
+    Args:
+        model_func (Callable): The model function to be traced.
+        input_sample (Tensor, optional): An input for tracing. Default: None.
+    """
+
+    if input_sample is None:
+        input_sample = torch.rand(1, 3, 640, 640)
+    input_sample = input_sample.to(device=model_func.device)
+
+    model = TraceWrapper(model_func)
+    model.eval()
+
+    trace_module = torch.jit.trace(model, input_sample)
+    trace_module.eval()
+
+    return trace_module
 
 
 def dict_to_tuple(out_dict: Dict[str, Tensor]) -> Tuple:
@@ -32,38 +69,3 @@ class TraceWrapper(nn.Module):
     def forward(self, x):
         out = self.model(x)
         return dict_to_tuple(out[0])
-
-
-@torch.no_grad()
-def get_trace_module(
-    model_func: Callable[..., nn.Module],
-    input_shape: Tuple[int, int] = (416, 416),
-):
-    """
-    Get the tracing of a given model function.
-
-    Example:
-
-        >>> from yolort.models import yolov5s
-        >>> from yolort.relaying.trace_wrapper import get_trace_module
-        >>>
-        >>> model = yolov5s(pretrained=True)
-        >>> tracing_module = get_trace_module(model)
-        >>> print(tracing_module.code)
-        def forward(self,
-            x: Tensor) -> Tuple[Tensor, Tensor, Tensor]:
-          _0, _1, _2, = (self.model).forward(x, )
-          return (_0, _1, _2)
-
-    Args:
-        model_func (Callable): The model function to be traced.
-        input_shape (Tuple[int, int]): Shape size of the input image.
-    """
-    model = TraceWrapper(model_func)
-    model.eval()
-
-    dummy_input = torch.rand(1, 3, *input_shape)
-    trace_module = torch.jit.trace(model, dummy_input)
-    trace_module.eval()
-
-    return trace_module
