ee282835345082b927cf496103472da59092f4e3
Your Name <you@example.com>
2022-03-09 14:57:57 +0800

    disable batchednms, it was wrong on multibatch fp16

--- yolort/relay/logits_decoder.py
+++ yolort/relay/logits_decoder.py
@@ -57,6 +57,7 @@ class LogitsDecoder(nn.Module):
 
         # The default boxes tensor has shape [batch_size, number_boxes, 4].
         boxes = torch.stack(bbox_regression)
-        boxes = boxes.unsqueeze(-2)
+        # uncomment is using BatchedNMSPlugin
+        # boxes = boxes.unsqueeze(-2)
         scores = torch.stack(pred_scores)
         return boxes, scores
